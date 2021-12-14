c498d1d79a5163e179727c36a8f53cdc05eaab18
Naveen V <velnaveen99@gmail.com>
2021-12-14 14:04:36 +0530

    Update mappingHandlers.ts

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,24 +1,18 @@
 import {StarterEntity} from "../types";
-//import {Balance} from "@polkadot/types/interfaces";
 import { Block, BlockID, BlockInfo, EventsByType, hashToHex } from '@terra-money/terra.js';
 
 export async function handleBlock(block: BlockInfo): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    //throw new Error('here');
-    let record = new StarterEntity(hashToHex(block.block_id.hash));
-    //Record block number
-    record.height = +block.block.header.height;
+    let record = new StarterEntity(block.block.header.height);
+    record.hash = hashToHex(block.block_id.hash);
     await record.save();
 }
 
-export async function handleEvent(event: EventsByType, block_id: BlockID): Promise<void> {
+export async function handleEvent(event: EventsByType, block: BlockInfo): Promise<void> {
     if('transfer' in event){
-        const record = await StarterEntity.get(hashToHex(block_id["hash"]));
+        const record = await StarterEntity.get(block.block.header.height);
         record.sender = event['transfer']['sender'];
         record.recipient = event['transfer']['recipient'];
         record.amount = event['transfer']['amount'];
         await record.save();
     }
 }
-
-
