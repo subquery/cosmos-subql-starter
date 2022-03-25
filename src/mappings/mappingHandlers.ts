46c3f46426637bd31b364e6c17be78decede1a22
James Bayly <bayly.james@gmail.com>
2022-03-25 16:33:24 +0100

    Update starter project

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,32 +1,56 @@
-import { StarterBlock, StarterTransfer } from "../types";
-import { hashToHex } from "@terra-money/terra.js/dist/util";
-import { TerraEvent, TerraBlock } from "@subql/types-terra";
+import { Block, Message, Transaction, TransferEvent } from "../types";
+import {
+  TerraEvent,
+  TerraBlock,
+  TerraMessage,
+  TerraTransaction,
+} from "@subql/types-terra";
+import { MsgExecuteContract } from "@terra-money/terra.js";
 
 export async function handleBlock(block: TerraBlock): Promise<void> {
-  const starterBlock = new StarterBlock(hashToHex(block.block.block_id.hash));
-  starterBlock.height = block.block.block.header.height;
-  await starterBlock.save();
+  const record = new Block(block.block.block_id.hash);
+  record.height = BigInt(block.block.block.header.height);
+  await record.save();
 }
 
-export async function handleTransferEvent(event: TerraEvent): Promise<void> {
-  const {
-    event: {
-      transfer: { sender, recipient, amount },
-    },
-  } = event;
+export async function handleTransaction(tx: TerraTransaction): Promise<void> {
+  const record = new Transaction(tx.tx.txhash);
+  record.blockHeight = BigInt(tx.block.block.block.header.height);
+  record.timestamp = tx.tx.timestamp;
+  await record.save();
+}
 
-  //logger.info(JSON.stringify(event.eventransfer));
+export async function handleMessage(
+  msg: TerraMessage<MsgExecuteContract>
+): Promise<void> {
+  const record = new Message(`${msg.tx.tx.txhash}-${msg.idx}`);
+  record.blockHeight = BigInt(msg.block.block.block.header.height);
+  record.txHash = msg.tx.tx.txhash;
+  record.contract = msg.msg.toData().contract;
+  record.sender = msg.msg.toData().sender;
+  record.executeMsg = JSON.stringify(msg.msg.toData().execute_msg);
+  await record.save();
+}
 
-  if (sender && recipient && amount) {
-    for (let i = 0; i < sender.length; i++) {
-      const starterTransfer = new StarterTransfer(
-        hashToHex(event.block.block_id.hash) + "-" + i
-      );
-      starterTransfer.blockId = hashToHex(event.block.block_id.hash);
-      starterTransfer.sender = sender[i];
-      starterTransfer.recipient = recipient[i];
-      starterTransfer.amount = amount[i];
-      await starterTransfer.save();
+export async function handleEvent(
+  event: TerraEvent<MsgExecuteContract>
+): Promise<void> {
+  const record = new TransferEvent(
+    `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`
+  );
+  for (const attr of event.event.attributes) {
+    switch (attr.key) {
+      case "sender":
+        record.sender = attr.value;
+        break;
+      case "recipient":
+        record.recipient = attr.value;
+        break;
+      case "amount":
+        record.amount = attr.value;
+        break;
+      default:
     }
   }
+  await record.save();
 }
