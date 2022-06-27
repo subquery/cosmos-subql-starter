32fb749709c1dbb407e5c81fa8a6e0d30d6c56b8
Scott Twiname <skott.twiname@gmail.com>
2022-06-27 21:33:35 +1200

    Update with breaking change

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -11,34 +11,32 @@ export async function handleBlock(block: CosmosBlock): Promise<void> {
 }
 
 export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
-  const transactionRecord = new Transaction(tx.hash);
-  transactionRecord.blockHeight = BigInt(tx.block.block.header.height);
-  transactionRecord.timestamp = tx.block.block.header.time;
+  const transactionRecord = Transaction.create({
+    id: tx.hash,
+    blockHeight: BigInt(tx.block.block.header.height),
+    timestamp: tx.block.block.header.time,
+  });
   await transactionRecord.save();
 }
 
 export async function handleMessage(msg: CosmosMessage): Promise<void> {
-  const messageRecord = new Message(`${msg.tx.hash}-${msg.idx}`);
-  messageRecord.blockHeight = BigInt(msg.block.block.header.height);
-  messageRecord.txHash = msg.tx.hash;
-  messageRecord.sender = msg.msg.sender;
-  messageRecord.contract = msg.msg.contract;
+  const messageRecord = Message.create({
+    id: `${msg.tx.hash}-${msg.idx}`,
+    blockHeight: BigInt(msg.block.block.header.height),
+    txHash: msg.tx.hash,
+    sender: msg.msg.decodedMsg.sender,
+    contract: msg.msg.decodedMsg.contract,
+  });
   await messageRecord.save();
 }
 
 export async function handleEvent(event: CosmosEvent): Promise<void> {
-  const eventRecord = new ExecuteEvent(
-    `${event.tx.hash}-${event.msg.idx}-${event.idx}`
-  );
-  eventRecord.blockHeight = BigInt(event.block.block.header.height);
-  eventRecord.txHash = event.tx.hash;
-  for (const attr of event.event.attributes) {
-    switch (attr.key) {
-      case "_contract_address":
-        eventRecord.contractAddress = attr.value;
-        break;
-      default:
-    }
-  }
+  const eventRecord = ExecuteEvent.create({
+    id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
+    blockHeight: BigInt(event.block.block.header.height),
+    txHash: event.tx.hash,
+    contractAddress: event.event.attributes.find(attr => attr.key === '_contract_address').value
+  });
+
   await eventRecord.save();
 }
