ae7e4cc4ad87791ca73947ccf061e7258a68111b
Bryan White <bryanchriswhite@gmail.com>
2022-10-04 14:42:01 +0200

    fix: swaps handler undefined checking (#122)

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -308,11 +308,13 @@ export async function handleLegacyBridgeSwap(msg: CosmosMessage<LegacyBridgeSwap
   logger.info(`[handleLegacyBridgeSwap] (tx ${msg.tx.hash}): indexing LegacyBridgeSwap ${id}`)
   logger.debug(`[handleLegacyBridgeSwap] (msg.msg): ${JSON.stringify(msg.msg, null, 2)}`)
 
-  const {
-    msg: {swap: {destination}},
-    funds: [{amount, denom}],
-    contract,
-  } = msg.msg.decodedMsg;
+  const contract = msg.msg.decodedMsg.contract;
+  const swapMsg = msg.msg.decodedMsg.msg;
+  const destination = swapMsg?.swap?.destination;
+
+  const funds = msg.msg.decodedMsg.funds || [];
+  const amount = funds[0]?.amount;
+  const denom = funds[0]?.denom;
 
   // gracefully skip indexing "swap" messages that doesn't fullfill the bridge contract
   // otherwise, the node will just crashloop trying to save the message to the db with required null fields.
