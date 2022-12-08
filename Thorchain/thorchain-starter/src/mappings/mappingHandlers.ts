295e7ded242058e4551f05c0f4cf8723ff9ccaf8
James Bayly <bayly.james@gmail.com>
2022-12-08 16:12:18 +1300

    Add THorchain

---
+++ Thorchain/thorchain-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,72 @@
+import { TransferEvent, Message, Transaction } from "../types";
+import {
+  CosmosEvent,
+  CosmosBlock,
+  CosmosMessage,
+  CosmosTransaction,
+} from "@subql/types-cosmos";
+
+/*
+export async function handleBlock(block: CosmosBlock): Promise<void> {
+  // If you want to index each block in Cosmos (CosmosHub), you could do that here
+}
+*/
+
+/*
+export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
+  // If you want to index each transaction in Cosmos (CosmosHub), you could do that here
+  const transactionRecord = Transaction.create({
+    id: tx.hash,
+    blockHeight: BigInt(tx.block.block.header.height),
+    timestamp: tx.block.block.header.time,
+  });
+  await transactionRecord.save();
+}
+*/
+
+export async function handleMessage(msg: CosmosMessage): Promise<void> {
+  logger.info("new Message");
+  logger.info(JSON.stringify(msg));
+
+  /*
+  const messageRecord = Message.create({
+    id: `${msg.tx.hash}-${msg.idx}`,
+    blockHeight: BigInt(msg.block.block.header.height),
+    txHash: msg.tx.hash,
+    from: msg.msg.decodedMsg.fromAddress,
+    to: msg.msg.decodedMsg.toAddress,
+    amount: JSON.stringify(msg.msg.decodedMsg.amount),
+  });
+  await messageRecord.save();
+  */
+}
+
+export async function handleEvent(event: CosmosEvent): Promise<void> {
+  logger.info("new Event");
+  logger.info(JSON.stringify(event));
+
+  /*
+  const eventRecord = new TransferEvent(
+    `${event.tx.hash}-${event.msg.idx}-${event.idx}`
+  );
+  eventRecord.blockHeight = BigInt(event.block.block.header.height);
+  eventRecord.txHash = event.tx.hash;
+
+  for (const attr of event.event.attributes) {
+    switch (attr.key) {
+      case "recipient":
+        eventRecord.recipient = attr.value;
+        break;
+      case "amount":
+        eventRecord.amount = attr.value;
+        break;
+      case "sender":
+        eventRecord.sender = attr.value;
+        break;
+      default:
+        break;
+    }
+  }
+  await eventRecord.save();
+  */
+}
