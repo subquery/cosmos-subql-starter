5944814499726b867ceca713bf18d3ca16517228
James Bayly <46693720+jamesbayly@users.noreply.github.com>
2022-12-13 14:03:40 +1300

    Thorchain (#17)
    * Add THorchain
    
    * Add more types
    
    * Finish Thorchain

---
+++ Thorchain/thorchain-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,73 @@
+import { Coin, Deposit, DepositCoin } from "../types";
+import { CosmosMessage } from "@subql/types-cosmos";
+
+type DepositMessage = {
+  coins: {
+    asset: {
+      chain: string;
+      symbol: string;
+      ticker: string;
+      synth: boolean;
+    };
+    amount: string;
+  }[];
+  memo: string;
+  signer: any;
+};
+
+/*
+export async function handleBlock(block: CosmosBlock): Promise<void> {
+  // If you want to index each block in Thorchain, you could do that here
+}
+*/
+
+/*
+export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
+  // If you want to index each transaction in Thorchain, you could do that here
+  const transactionRecord = Transaction.create({
+    id: tx.hash,
+    blockHeight: BigInt(tx.block.block.header.height),
+    timestamp: tx.block.block.header.time,
+  });
+  await transactionRecord.save();
+}
+*/
+
+export async function handleMessage(
+  msg: CosmosMessage<DepositMessage>
+): Promise<void> {
+  // Create Deposit record
+  const depositEntity = Deposit.create({
+    id: `${msg.tx.hash}-${msg.idx}`,
+    blockHeight: BigInt(msg.block.block.header.height),
+    txHash: msg.tx.hash,
+    signer: msg.msg.decodedMsg.signer.toString(),
+    memo: msg.msg.decodedMsg.memo,
+  });
+  await depositEntity.save();
+
+  // Iterate through coins
+  for (let coin of msg.msg.decodedMsg.coins) {
+    // Check if the coin exists
+    let coinEntity = await Coin.get(`${coin.asset.chain}-${coin.asset.symbol}`);
+    if (!coinEntity) {
+      // Does not exist, create
+      coinEntity = Coin.create({
+        id: `${coin.asset.chain}-${coin.asset.symbol}`,
+        chain: coin.asset.chain,
+        symbol: coin.asset.symbol,
+        ticker: coin.asset.ticker,
+        synth: coin.asset.synth,
+      });
+      await coinEntity.save();
+    }
+
+    // Create Deposit Coin link
+    await DepositCoin.create({
+      id: `${msg.tx.hash}-${msg.idx}-${coin.asset.chain}-${coin.asset.symbol}`,
+      depositId: depositEntity.id,
+      coinId: coinEntity.id,
+      amount: BigInt(coin.amount),
+    }).save();
+  }
+}
