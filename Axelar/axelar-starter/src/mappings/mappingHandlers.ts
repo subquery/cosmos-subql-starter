4a8bbd96353080e5a68eca96ba74acc6456f03b1
seandotau <seandotau@gmail.com>
2022-12-23 11:59:23 +1100

    Added types

--- Axelar/axelar-starter/src/mappings/mappingHandlers.ts
+++ Axelar/axelar-starter/src/mappings/mappingHandlers.ts
@@ -1,10 +1,18 @@
-import { TransferEvent, Message } from "../types";
+import {DepositConfirmation} from "../types";
 import {
   CosmosEvent,
   CosmosBlock,
   CosmosMessage,
   CosmosTransaction,
 } from "@subql/types-cosmos";
+
+type depositConfirmationMsg = {
+  type: string;
+  attributes: {        
+    key: string;
+    value: string;
+  }[];
+}
 
 /*
 export async function handleBlock(block: CosmosBlock): Promise<void> {
@@ -22,9 +30,9 @@ export async function handleTransaction(tx: CosmosTransaction): Promise<void> {
   });
   await transactionRecord.save();
 }
-*/
 
 export async function handleMessage(msg: CosmosMessage): Promise<void> {
+  logger.warn("in message handler")
   const messageRecord = Message.create({
     id: `${msg.tx.hash}-${msg.idx}`,
     blockHeight: BigInt(msg.block.block.header.height),
@@ -33,25 +41,29 @@ export async function handleMessage(msg: CosmosMessage): Promise<void> {
     to: msg.msg.decodedMsg.toAddress,
     amount: JSON.stringify(msg.msg.decodedMsg.amount),
   });
+  logger.warn("** blockHeight is: " + messageRecord.blockHeight)
   await messageRecord.save();
 }
+*/
 
 export async function handleEvent(event: CosmosEvent): Promise<void> {
-  const eventRecord = new TransferEvent(
+  logger.warn("in event handler: " + JSON.stringify(event.event))
+  const eventRecord = new DepositConfirmation(
     `${event.tx.hash}-${event.msg.idx}-${event.idx}`
   );
   eventRecord.blockHeight = BigInt(event.block.block.header.height);
   eventRecord.txHash = event.tx.hash;
+
   for (const attr of event.event.attributes) {
     switch (attr.key) {
-      case "recipient":
-        eventRecord.recipient = attr.value;
+      case "sourceChain":
+        eventRecord.sourceChain = attr.value;
         break;
       case "amount":
-        eventRecord.amount = attr.value;
+        eventRecord.depositAddress = attr.value;
         break;
       case "sender":
-        eventRecord.sender = attr.value;
+        eventRecord.destinationChain = attr.value;
         break;
       default:
         break;
