568e9783b26729d90605363d951c6dce098adabf
web3cdn <info@web3cdn.services>
2023-09-08 13:26:30 +0300

    Umee Cosmos Subql Starter

---
+++ Umee/umee-starter/src/types/CosmosMessageTypes.ts
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Auto-generated , DO NOT EDIT
+import {CosmosMessage} from "@subql/types-cosmos";
+
+import {MsgUnjail} from "./proto-interfaces/cosmos/slashing/v1beta1/tx";
+
+import {MsgVoteWeighted} from "./proto-interfaces/cosmos/gov/v1beta1/tx";
+
+import {WeightedVoteOption} from "./proto-interfaces/cosmos/gov/v1beta1/gov";
+
+
+export type MsgUnjailMessage = CosmosMessage<MsgUnjail>;
+
+export type MsgVoteWeightedMessage = CosmosMessage<MsgVoteWeighted>;
+
+export type WeightedVoteOptionMessage = CosmosMessage<WeightedVoteOption>;
