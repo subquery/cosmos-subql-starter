7052e266f0e6b35295f5c4b6a7257d0bce116a7b
bz888 <ziyu.ben158@gmail.com>
2023-10-11 15:01:23 +1300

    add project.ts

---
+++ Stargaze/stargaze-starter/project.ts
@@ -0,0 +1,99 @@
+import {
+  SubqlCosmosDatasourceKind,
+  SubqlCosmosHandlerKind,
+  CosmosProject,
+} from "@subql/types-cosmos";
+
+// Can expand the Datasource processor types via the genreic param
+const project: CosmosProject = {
+  specVersion: "1.0.0",
+  version: "0.0.1",
+  name: "stargaze-starter",
+  description:
+    "This project can be use as a starting point for developing your Cosmos persistence based SubQuery project",
+  runner: {
+    node: {
+      name: "@subql/node-cosmos",
+      version: ">=3.0.0",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    /* The genesis hash of the network (hash of block 0) */
+    chainId: "stargaze-1",
+    /**
+     * These endpoint(s) should be non-pruned archive nodes
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     * We suggest providing an array of endpoints for increased speed and reliability
+     */
+    endpoint: ["https://rpc-persistent-ia.cosmosia.notional.ventures/"],
+    // # Optionally provide the HTTP endpoint of a full chain dictionary to speed up processing
+    chaintypes: new Map([
+      [
+        "cosmos.slashing.v1beta1",
+        {
+          file: "./proto/cosmos/slashing/v1beta1/tx.proto",
+          messages: ["MsgUnjail"],
+        },
+      ],
+      [
+        "cosmos.gov.v1beta1",
+        {
+          file: "./proto/cosmos/gov/v1beta1/tx.proto",
+          messages: ["MsgVoteWeighted"],
+        },
+      ],
+      [
+        "cosmos.gov.v1beta1.gov",
+        {
+          file: "./proto/cosmos/gov/v1beta1/gov.proto",
+          messages: ["WeightedVoteOption"],
+        },
+      ],
+      [
+        "publicawesome.stargaze.claim.v1beta1",
+        {
+          file: "./proto/stargaze/claim/v1beta1/tx.proto",
+          messages: ["MsgInitialClaim"],
+        },
+      ],
+    ]),
+  },
+  dataSources: [
+    {
+      kind: SubqlCosmosDatasourceKind.Runtime,
+      startBlock: 6000000,
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            handler: "handleEvent",
+            kind: SubqlCosmosHandlerKind.Event,
+            filter: {
+              type: "execute",
+              messageFilter: {
+                type: "/cosmwasm.wasm.v1.MsgExecuteContract",
+              },
+            },
+          },
+          {
+            handler: "handleEvent",
+            kind: SubqlCosmosHandlerKind.Message,
+            filter: {
+              type: "/cosmwasm.wasm.v1.MsgExecuteContract",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default project;
