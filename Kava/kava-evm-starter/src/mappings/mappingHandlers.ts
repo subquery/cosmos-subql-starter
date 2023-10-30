602e13716e925a92e269933fa3b189824e62eb5f
James Bayly <bayly.james@gmail.com>
2023-10-30 11:08:55 +0800

    Add Kava EVM project

---
+++ Kava/kava-evm-starter/src/mappings/mappingHandlers.ts
@@ -0,0 +1,36 @@
+import assert from "assert";
+import { Approval, Transaction } from "../types";
+import {
+  ApproveTransaction,
+  TransferLog,
+} from "../types/abi-interfaces/Erc20Abi";
+
+export async function handleLog(transferLog: TransferLog): Promise<void> {
+  logger.info("transaction: " + transferLog.transactionHash);
+  assert(transferLog.args, "Expected args to exist");
+  const transaction = Transaction.create({
+    id: transferLog.transactionHash,
+    value: transferLog.args.value.toBigInt(),
+    from: transferLog.args.from,
+    to: transferLog.args.to,
+    contractAddress: transferLog.address,
+  });
+
+  await transaction.save();
+}
+
+export async function handleTransaction(
+  approveCallTransaction: ApproveTransaction
+): Promise<void> {
+  logger.info("approval: " + approveCallTransaction.hash);
+  assert(approveCallTransaction.args, "Expected args to exist");
+  const approval = Approval.create({
+    id: approveCallTransaction.hash,
+    owner: approveCallTransaction.from,
+    value: BigInt((await approveCallTransaction.args[1]).toString()),
+    spender: (await approveCallTransaction.args[0]).toString(),
+    contractAddress: approveCallTransaction.to,
+  });
+
+  await approval.save();
+}
