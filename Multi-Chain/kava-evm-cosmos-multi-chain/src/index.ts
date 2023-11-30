17c8ac9cf044eec86811f2dd7586c71ecdb2d457
godlin <bogodline@gmail.com>
2023-11-30 15:01:53 +0100

    Structure Improved

--- Multi-Chain/kava-evm-cosmos-multi-chain/src/index.ts
+++ Multi-Chain/kava-evm-cosmos-multi-chain/src/index.ts
@@ -1,2 +1,7 @@
+import { atob } from "abab";
+
+if (!global.atob) {
+  global.atob = atob as any;
+}
 //Exports all handler functions
 export * from "./mappings/mappingHandlers";
--- Multi-Chain/kava-evm-cosmos-multi-chain/src/mappings/mappingHandlers.ts
+++ Multi-Chain/kava-evm-cosmos-multi-chain/src/mappings/mappingHandlers.ts
@@ -1,50 +1,83 @@
-import { Transfers, Approval, Transaction } from "../types";
+import { EVMTransfers, CosmosTransfers, Address } from "../types";
 import { CosmosEvent } from "@subql/types-cosmos";
+import { ethers } from "ethers";
+import bech32 from "bech32";
 import assert from "assert";
 import {
   ApproveTransaction,
   TransferLog,
 } from "../types/abi-interfaces/Erc20Abi";
 
+function kavaToEthAddress(kavaAddress: string) {
+  return ethers.utils.getAddress(
+    ethers.utils.hexlify(bech32.fromWords(bech32.decode(kavaAddress).words))
+  );
+}
+
+function ethToKavaAddress(ethereumAddress: string) {
+  return bech32.encode(
+    "kava",
+    bech32.toWords(
+      ethers.utils.arrayify(ethers.utils.getAddress(ethereumAddress))
+    )
+  );
+}
+
+async function checkGetUserAddress(
+  cosmosAddress: string,
+  evmAddress: string
+): Promise<Address> {
+  let addressId = `${cosmosAddress}-${evmAddress}`;
+  let userRecord = await Address.get(addressId);
+  if (!userRecord) {
+    userRecord = Address.create({
+      id: addressId,
+      cosmosAddress: cosmosAddress,
+      evmAddress: evmAddress,
+    });
+    await userRecord.save();
+  }
+  return userRecord;
+}
+
 export async function handleEVMLog(transferLog: TransferLog): Promise<void> {
   logger.info("transaction: " + transferLog.transactionHash);
+  let from = transferLog.transaction.from.toString();
+  let to = transferLog.transaction.to.toString();
+  let contractAddress = transferLog.address;
   assert(transferLog.args, "Expected args to exist");
-  const transaction = Transaction.create({
+  const transaction = EVMTransfers.create({
     id: transferLog.transactionHash,
     value: transferLog.args.value.toBigInt(),
-    from: transferLog.args.from,
-    to: transferLog.args.to,
-    contractAddress: transferLog.address,
+    fromId: (
+      await checkGetUserAddress(ethToKavaAddress(from), from)
+    ).id.toString(),
+    toId: (await checkGetUserAddress(ethToKavaAddress(to), to)).id.toString(),
+    contractAddressId: (
+      await checkGetUserAddress(
+        ethToKavaAddress(contractAddress),
+        contractAddress
+      )
+    ).id.toString(),
   });
 
   await transaction.save();
 }
 
-export async function handleEVMTransaction(
-  approveCallTransaction: ApproveTransaction
-): Promise<void> {
-  logger.info("approval: " + approveCallTransaction.hash);
-  assert(approveCallTransaction.args, "Expected args to exist");
-  const approval = Approval.create({
-    id: approveCallTransaction.hash,
-    owner: approveCallTransaction.from,
-    value: BigInt((await approveCallTransaction.args[1]).toString()),
-    spender: (await approveCallTransaction.args[0]).toString(),
-    contractAddress: approveCallTransaction.to,
-  });
-
-  await approval.save();
-}
-
 export async function handleCosmosEvent(event: CosmosEvent): Promise<void> {
   logger.info(`New transfer event at block ${event.block.block.header.height}`);
-
-  const newTransfers = Transfers.create({
+  let from = event.msg.msg.decodedMsg.fromAddress;
+  let to = event.msg.msg.decodedMsg.toAddress;
+  const newTransfers = CosmosTransfers.create({
     id: `${event.tx.hash}-${event.msg.idx}-${event.idx}`,
     blockHeight: BigInt(event.block.block.header.height),
     txHash: event.tx.hash,
-    fromAddress: event.msg.msg.decodedMsg.fromAddress,
-    toAddress: event.msg.msg.decodedMsg.toAddress,
+    fromAddressId: (
+      await checkGetUserAddress(from, kavaToEthAddress(from))
+    ).id.toString(),
+    toAddressId: (
+      await checkGetUserAddress(to, kavaToEthAddress(to))
+    ).id.toString(),
     amount: event.msg.msg.decodedMsg.amount[0].amount,
     denomination: event.msg.msg.decodedMsg.amount[0].denom,
   });
